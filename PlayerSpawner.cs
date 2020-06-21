115ec0146f7a5cfa90b9b2dcdb56289ac9dfb445
Oleh Stelmashchuk <38110445+Oleh9876@users.noreply.github.com>
2020-06-21 22:52:51 +0200

    spawning players

---
+++ PlayerSpawner.cs
@@ -0,0 +1,19 @@
+﻿using Cinemachine;
+using Photon.Pun;
+using UnityEngine;
+
+namespace EnemyAttack
+{
+    public class PlayerSpawner : MonoBehaviourPun
+    {
+        [SerializeField] private GameObject playerPrefab = null;
+        [SerializeField] private CinemachineVirtualCamera playerCamera = null;
+       
+        private void Start()
+        {
+            var player = PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero, Quaternion.identity);
+            playerCamera.Follow = player.transform;
+            playerCamera.LookAt = player.transform;
+        }
+    }
+}
