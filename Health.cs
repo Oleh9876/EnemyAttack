c38fd623a2b6c7b321e214b34f579048385cbe85
Oleh Stelmashchuk <38110445+Oleh9876@users.noreply.github.com>
2020-06-21 22:39:59 +0200

    Health script creating

---
+++ Health.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using EnemyAttack.Movement;
+
+public class Health : MonoBehaviourPunCallbacks, Photon.Pun.IPunObservable
+{
+    
+    public float health = 100;
+    private Animator animator;
+     bool isDead;
+    
+    
+    
+    
+void Awake()
+    {
+     animator = GetComponent<Animator>();
+
+         
+ }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(health);
+        }
+        else
+        {
+            health = (float)stream.ReceiveNext();
+
+        }
+    }
+
+
+    public void TakeDamage(float damage)
+    {
+    	
+        
+        health -= damage;
+       
+        
+ 
+        
+        	if (health <=0 && !isDead)
+        {
+        	Death();
+        }
+
+    }
+     
+    void Death()
+{
+	isDead=true;
+	animator.SetTrigger("Die");
+
+}
+
+
+    IEnumerator Respawn()
+    {
+    	isDead=false;
+
+        health = 100;
+        GetComponent<Movement>().enabled = false;
+        transform.position = new Vector3(0, 0, 0);
+        yield return new WaitForSeconds(5);
+        GetComponent<Movement>().enabled = true;
+        
+    }
+
+    
+    
+
+    void Update()
+    {
+        
+         
+        if (health<=0)
+        {
+        	StartCoroutine(Respawn());
+        }
+
+    }
+}
+
