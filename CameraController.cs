dc4cd19250bdeeecd7bbafea03b2af0fe0b550e9
Oleh Stelmashchuk <38110445+Oleh9876@users.noreply.github.com>
2020-06-21 22:40:36 +0200

    Camera Controller script

---
+++ CameraController.cs
@@ -0,0 +1,27 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+  
+    [SerializeField]
+    public float mouseSensitivity = 0.1f;
+    private CinemachineComposer composer;
+   private void Start()
+    {
+        composer = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineComposer>();
+
+    }
+
+   
+   private void Update()
+    {
+        float vertical = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        composer.m_TrackedObjectOffset.y += vertical;
+        composer.m_TrackedObjectOffset.y = Mathf.Clamp(composer.m_TrackedObjectOffset.y, -5, 5);
+
+
+    }
+}
